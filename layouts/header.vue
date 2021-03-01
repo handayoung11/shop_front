ab30bff193e7102ac50423c001d835f498afa499
JinYeongKim <handayoung11@gmail.com>
2021-03-02 02:35:24 +0900

    login창 디자인

--- layouts/header.vue
+++ layouts/header.vue
@@ -13,17 +13,25 @@
         <!--        </b-nav-item-dropdown>-->
 
         <b-nav-item-dropdown text="User" right>
-          <b-dropdown-item href="#">Sign in</b-dropdown-item>
+          <b-dropdown-item v-b-modal.login-modal href="#"
+            >Sign in</b-dropdown-item
+          >
           <b-dropdown-item href="#">Sign up</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-navbar>
+    <login />
   </div>
 </template>
 
 <script>
+import login from '@/layouts/login'
 export default {
   name: 'Header',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    login,
+  },
 }
 </script>
 
---
+++ layouts/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-modal
+      id="login-modal"
+      title="Login"
+      ok-only
+      ok-title="Cancel"
+      ok-variant="secondary"
+    >
+      <el-input v-model="loginId" placeholder="Input your ID"></el-input>
+      <el-input
+        v-model="loginPW"
+        placeholder="Input your password"
+        style="margin-top: 15px"
+      ></el-input>
+      <el-button type="primary" round>Sign in</el-button>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginId: '',
+      loginPW: '',
+    }
+  },
+}
+</script>
+
+<style>
+.el-input__inner {
+  width: 100%;
+}
+</style>
