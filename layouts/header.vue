508a8aef67596c479f1cd57a6977e109b06aa6e4
JinYeongKim <handayoung11@gmail.com>
2021-03-02 01:01:04 +0900

    공통 header 작성

---
+++ layouts/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item href="/">Home</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+        <!--        <b-nav-item-dropdown text="Lang" right>-->
+        <!--          <b-dropdown-item href="#">EN</b-dropdown-item>-->
+        <!--          <b-dropdown-item href="#">ES</b-dropdown-item>-->
+        <!--          <b-dropdown-item href="#">RU</b-dropdown-item>-->
+        <!--          <b-dropdown-item href="#">FA</b-dropdown-item>-->
+        <!--        </b-nav-item-dropdown>-->
+
+        <b-nav-item-dropdown text="User" right>
+          <b-dropdown-item href="#">Sign in</b-dropdown-item>
+          <b-dropdown-item href="#">Sign up</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!--<style scoped></style>-->
